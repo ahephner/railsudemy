75ae1fca35c7979fffd23ff3459b1d86e8440b81
ahephner <ajhephner178@gmail.com>
2018-10-01 16:43:28 -0400

    seed file understood

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |x|
+  Blog.create!(
+      title: "My Post #{x}", 
+      body:  "Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+         consequat. Duis aute irure dolor in reprehenderit in voluptate velit"
+  )
+end 
+
+#puts is equivalent to console.log or print
+puts "blogs made"
+
+5.times do |i|
+    Skill.create!(
+        title: "Skill #{i}",
+        percent_used: "#{i}"
+    )
+end
+
+puts "skills added"
+
+
+9.times do |port|
+    Portfolio.create!(
+        title: "My project #{port}",
+        subtitle: "I did it",
+        body: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+        nostrud exercitation ullamco laboris nisi ut aliquip", 
+        main_image: "https://via.placeholder.com/330x150", 
+        second_image:  "https://via.placeholder.com/100x50"
+    )
+end
+
+puts "9 portfolio made"
