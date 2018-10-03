3bfcebd38968654d7c4a3b89f6b48592bbd74a13
ahephner <ajhephner178@gmail.com>
2018-10-03 15:29:39 -0400

    simple route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
   #encapsulates all popular routes apps use. in terminal run rake routes to see 
-  resources :blogs
+  resources :blogs do
+    member do
+      post :toggle_status
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
