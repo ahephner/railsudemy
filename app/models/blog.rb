8c4a59e0afb4593c2281bf7b4982a95bf1f6b200
ahephner <ajhephner178@gmail.com>
2018-10-03 14:56:56 -0400

    added some comments on creating enum

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
+  #this acts like a flag yes or no sets all kind of optons https://api.rubyonrails.org/v5.2.1/classes/ActiveRecord/Enum.html
   enum status:{draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
--- db/migrate/20181003183135_add_post_status_blogs.rb
+++ db/migrate/20181003183135_add_post_status_blogs.rb
@@ -3,3 +3,15 @@ class AddPostStatusBlogs < ActiveRecord::Migration[5.2]
     add_column :blogs, :status, :integer, default: 0 
   end
 end
+
+# Creating the enum for published or not
+# 1. rails g migration add_posts_blogs status:integer
+# 2. open this file add the add_column line !Important i had to physically type it default jsut means where to start
+# 3. run rails db:migrate
+# 4. open blogs model 
+# 5. add enum status{draft:1, publish:2}
+# 6. rails c
+# 7. Blogs.create!(title: 'test', body:'you got it') -> see if enum worked in output
+# 8. Blogs.last.published set one to publish
+# 9. Blogs.published.count -> 1 if working right
+# 10. Blogs.draft.count 
