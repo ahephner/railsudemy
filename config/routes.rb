1fe8626b565ca7e3ef1318f61a689d257c6b6e9f
ahephner <ajhephner178@gmail.com>
2018-10-03 14:14:42 -0400

    Revert "added custom routes to portfolio and pages"
    This reverts commit 773266295bf4ba68c7932879d050318a28d2bcb2.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
-  #, except: [:show] -stops the route from using the resource
-  resources :portfolios, except: [:show]
-  #custom route for show very similar to pages. 
-  #the as: is what you have to use on the view <%= link_to 'i am a link', portfolio_show_path(x)%> 
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  #map route
-  #can put whatever you want after get as long as the to: is correct       
-  get 'about-me', to: 'pages#about'
-  #replace get 'pages/about'
-  get 'contact', to: 'pages#contact'
-
+  resources :portfolios
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
   #encapsulates all popular routes apps use. in terminal run rake routes to see 
   resources :blogs
-  #set root path to application in this case the home page could be called anything
-  #localhost:3000 now goes to the home page
-  root to: 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
