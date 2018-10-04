88a747499c022026ea9dfa8afef5d5c2fe8921a0
ahephner <ajhephner178@gmail.com>
2018-10-04 13:40:59 -0400

    added scoping

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,12 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :second_image
+#1
+    def self.sql
+        where(subtitle: "SQL")
+    end
+#2
+    scope :ruby_on_rails, -> {where(subtitle: "Ruby")}
 end
+
+#1-Scope here we are setting what to return now go to controller and def index @port_items = Portfolio.sql will render only items with subtitle = SQL 
+#2-Second way to scope
