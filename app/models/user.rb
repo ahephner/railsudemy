9ad308dcf0293b3f3d7e24365403eda549be4f51
ahephner <ajhephner178@gmail.com>
2018-10-16 16:27:49 -0400

    added logged welcome statement

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name
+#this allows us to grab name for displaying on screen 
+#like Hello AJ
+#self.name = this.name 
+#split.first out of box works on space so AJ H first is aj
+  def first_name
+    self.name.split.first
+  end
+  
+  #split.last gives last value if there is only 1 value you still get that value here
+  def last_name
+    self.name.split.last
+  end 
 end
+
+
 
 #:confirmable have to click link sent in email first
 # :lockable lockout after so many failed logins
