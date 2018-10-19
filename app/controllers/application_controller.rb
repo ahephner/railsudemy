3c26ff7338d21932b9a50ebbcea04ceecdb36a81
ahephner <ajhephner178@gmail.com>
2018-10-19 16:37:48 -0400

    added usertype to concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
 class ApplicationController < ActionController::Base
     include DeviseWhiteList
     include SetSource
+    include UserType
     #this is the main controller this before_action says run this before anything else
     #since we dont get devise controller this says if: youre the devise controller do this
     
     
-    #super means dont change anything this is saying if current_user is true just use that else use other thing
-    #current user was from devise
-    def current_user
-        super || OpenStruct.new(name: "Guest", first_name: "Guest", last_name: 'User', email: 'some@test.com')
-    end    
+    
  
 end
