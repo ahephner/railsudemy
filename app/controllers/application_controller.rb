ccdeec5e2bc848fdf9295ebec1a85a7d2d4894f2
ahephner <ajhephner178@gmail.com>
2018-10-19 15:23:16 -0400

    added session information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
     #this is the main controller this before_action says run this before anything else
     #since we dont get devise controller this says if: youre the devise controller do this
     before_action :configure_permitted_params, if: :devise_controller?
-
+    before_action :set_source
+    #set source of the session store information in cookie form and have access anywhere. 
+    def set_source
+        session[:source] = params[:q] if params[:q]
+    end
     #allow us to accept more information at login than  what devise comes out of the box with
     def configure_permitted_params
                                         #route      #what to accept
