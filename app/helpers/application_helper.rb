e5dbecdbf076dffacf3bc8661e39568c8dc91f43
ahephner <ajhephner178@gmail.com>
2018-12-17 16:55:23 -0500

    home page layout done, nav bar updated and buttons working!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
-    def login_helper
+    #the style allows us to pass in dynamic classes based on different values 
+    #look at _application_nav.html.erb to see how i passed in the 'nav-link style from the home page'
+    def login_helper style
         if current_user.is_a?(GuestUser) 
-            (link_to "Register", new_user_registration_path) +
-            "<br>".html_safe +
-            (link_to "Login", new_user_session_path)  
+            (link_to "Register", new_user_registration_path, class: style) +
+            " ".html_safe +
+            (link_to "Login", new_user_session_path, class: style)  
         else 
-            link_to "Logout",destroy_user_session_path, method: :delete
+            link_to "Logout",destroy_user_session_path,style, method: :delete
         end   
     end    
 
